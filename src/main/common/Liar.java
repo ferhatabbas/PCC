bdc2ba1c2443844b3413d0afa1bad3bc78808b20
Albonite <anedefeu@hotmail.com>
2015-11-19 14:56:31 -0500

    Debut requestHandler + password encryption

--- src/main/common/Liar.java
+++
@@ -1,7 +0,0 @@
-package main.common;
-
-/**
- * Created by ferhat on 2015-11-12.
- */
-public class Liar {
-}
---
+++ src/main/server/Server.java
@@ -0,0 +1,31 @@
+package main.server;
+
+/**
+ * Created by Sylvain on 2015-11-19.
+
+public class Server {
+
+   private MessageServ message;
+
+    public MessageServ RequestHandler(MessageServ MessageEntree){
+        String requete = MessageEntree.getrequete();
+        switch (requete){
+            case("CONNECT"):
+                break;
+            case("LOGIN"):
+                break;
+            case("USERS"):
+                break;
+            case("ACTION"):
+                break;
+            case("NEWCOUPLE"):
+                break; // liste ID,liste prenom,liste nom,liste sexe
+
+
+        }
+
+
+
+    }
+}
+**/
