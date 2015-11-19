11017789c217632668abf0841b1f77d530320a85
Albonite <anedefeu@hotmail.com>
2015-11-19 15:17:53 -0500

    Encryption termine. Voir test

---
+++ src/test/TestSylvain.java
@@ -0,0 +1,33 @@
+package test;
+
+
+import junit.framework.TestCase;
+import main.server.Database;
+import main.server.PasswordService;
+
+/**
+ * Created by ferhat on 2015-11-18.
+ */
+public class TestSylvain extends TestCase {
+
+
+    public void testpassword() throws  Exception{
+       PasswordService ps = new PasswordService();
+        
+        String essai = "toto";
+        essai = ps.encrypt(essai);
+        System.out.println(essai);
+
+        String essai2 = "toto";
+        essai2 = ps.encrypt(essai2);
+        System.out.println(essai2);
+        if(essai2.equals(essai))
+            System.out.println("TRUE");
+        else
+            System.out.println("FALSE");
+
+
+    }
+
+
+}
